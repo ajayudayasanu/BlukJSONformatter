d27aed20cd1fdc031aae69a766cb2ed6a0563d71
Ajay U <ajayudayasanu95@gmail.com>
2018-10-28 23:35:39 +0530

    first commit

---
+++ MyJSON formatter/MyJSON formatter/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace MyJSON_formatter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.unformatted_txt = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.countlbl = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // unformatted_txt
+            // 
+            this.unformatted_txt.Location = new System.Drawing.Point(215, 34);
+            this.unformatted_txt.Name = "unformatted_txt";
+            this.unformatted_txt.Size = new System.Drawing.Size(446, 105);
+            this.unformatted_txt.TabIndex = 1;
+            this.unformatted_txt.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Copy un formated json";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "formatted JSON";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(375, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // countlbl
+            // 
+            this.countlbl.AutoSize = true;
+            this.countlbl.Location = new System.Drawing.Point(776, 77);
+            this.countlbl.Name = "countlbl";
+            this.countlbl.Size = new System.Drawing.Size(47, 17);
+            this.countlbl.TabIndex = 5;
+            this.countlbl.Text = "count:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 264);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(123, 264);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(583, 303);
+            this.panel1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(442, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(837, 137);
+            this.richTextBox1.MinimumSize = new System.Drawing.Size(200, 100);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(300, 200);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(601, 168);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1014, 579);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.countlbl);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.unformatted_txt);
+            this.MinimumSize = new System.Drawing.Size(200, 100);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox unformatted_txt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label countlbl;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ MyJSON formatter/MyJSON formatter/Form1.cs
@@ -0,0 +1,105 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyJSON_formatter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static string FormatJson(string json)
+        {
+            try
+            {
+                dynamic parsedJson = JsonConvert.DeserializeObject(json);
+                return JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
+                }
+            catch
+            {
+                MessageBox.Show("error");
+                return null;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string untxted = unformatted_txt.Text;
+            //string formattedtxt = FormatJson(untxted);
+            // formatted_txt.Text = formattedtxt;
+            string unformattedtxt = unformatted_txt.Text;
+            string[] splitedtxt = unformattedtxt.Split((char)10);
+            int count = unformattedtxt.Split((char)10).Length;
+            countlbl.Text = "count: " + count;
+            //label3.Text = splitedtxt[1];
+            List<string> formattedList = new List<string>();
+            
+            for (int i=0; i< count;i++)
+            {
+                formattedList.Add(FormatToJson(splitedtxt[i]));
+            }
+
+            label4.Text = formattedList[0];
+          
+            string FormatToJson(string inputtxt)
+            {
+                string Formated_JSON = "";
+                Formated_JSON = FormatJson(inputtxt);
+                return Formated_JSON;
+            }
+
+            int pointX = 30;
+            int pointY = 40;
+            int NewHeight = 200;
+
+            for (int i = 0; i < count; i++)
+            {
+
+                RichTextBox a = new RichTextBox();
+                a.Width = 400;
+                a.Location = new Point(pointX, pointY);
+                a.Multiline = true;
+                a.Text = (formattedList[i]).ToString();                             
+                panel1.Controls.Add(a);
+                panel1.Show();
+                panel1.MaximumSize = new Size(panel1.Width, NewHeight);
+                panel1.Size = new Size(panel1.Height, NewHeight);
+                NewHeight += 200;
+                pointY += 100;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            int NewHeight = 200;
+            panel1.MaximumSize = new Size(panel1.Width, NewHeight);
+            panel1.Size = new Size(panel1.Height, NewHeight);
+        }
+    }
+}
